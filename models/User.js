317f7685fba4f6f2a58d0468c986e09a2f0d029f
raposamillar <lisa.raposamillar@gmail.com>
2022-10-28 23:04:16 -0400

    create Thought model and ReactionSchema

--- models/User.js
+++ models/User.js
@@ -1,27 +1,24 @@
 const { Schema, model } = require('mongoose');
 
-const UserSchema = new Schema({
+const UserSchema = new Schema ({
   username: {
     type: String,
     unique: true,
     required: 'Please provide a username.',
     trim: true
   },
-
   email: {
     type: String,
     required: 'Please provide a email.',
     unique: true,
     match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Please enter a valid email address.']
   },
-
   thoughts: [
     {
       type: Schema.Types.ObjectId,
       ref: 'Thought'
     }
   ],
-
   friends: [
     {
       type: Schema.Types.ObjectId,
