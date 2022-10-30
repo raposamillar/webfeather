18b28878da708d65430dce8350c27a643ab6530e
raposamillar <lisa.raposamillar@gmail.com>
2022-10-30 19:36:32 -0400

    require routes in server.js and add public directory

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/webfeather', {
   useNewUrlParser: true,
